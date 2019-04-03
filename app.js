15bd4a928e4dbba005f4847f994e089116499797
Sanjana Hathwar <31928996+SanjanaHathwar@users.noreply.github.com>
2019-04-03 22:48:15 +0530

    TRAINER

---
+++ app.js
@@ -0,0 +1,8 @@
+var express=require('express');
+var app=express();
+var port = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+
+app.listen(port);
+console.log("Express Started on Port : "+port); 
